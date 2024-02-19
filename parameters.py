254d3b449e2bbf58ca4050c5c1aaa3b161a2e576
rindtorff <ermi.rindtorff@web.de>
2024-02-19 20:22:51 +0100

    outsourced parameters in own file

---
+++ parameters.py
@@ -0,0 +1,7 @@
+EXAMPLE_LIST = ['toy']  # 'toy', 'grid', 'ring'
+T = 60
+TIMELIMIT_LIST = [30, 50, 60, 70, 90]
+TIME_INCREASE = 60  # increase time by value if not improved
+PATIENCE = 11  # number of allowed cons. non-improvements
+MAXTIME = 120  # in seconds
+PLOT_RESULTS = True
